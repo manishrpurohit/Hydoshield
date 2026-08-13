9f53a73b80bce44b99385958547e65449c8ee6ce
Manish <manish22rp@gmail.com>
2026-08-13 23:50:52 +0530

    update app

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def api_predict():
 
 if __name__ == "__main__":
     logger.info("Starting Flask application...")
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="http://127.0.0.1:5000", port=5000)
